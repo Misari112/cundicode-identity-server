212e68e56634789142d0fc1bf3c859e9ddef9fc0
Misari112 <84647036+Misari112@users.noreply.github.com>
2023-04-25 22:54:04 -0500

    add

---
+++ Migrations/20230426035315_InitialMigrationAzure.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dis_identityserver.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigrationAzure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
